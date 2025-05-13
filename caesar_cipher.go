a7c9c73124591ac2cb31f8a2394b1801c7c8879e
Ryan Golang <165095177+ryanGOlang@users.noreply.github.com>
2025-05-13 21:11:34 +0100

    Create caesar_cipher.go

---
+++ caesar_cipher.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func E(p string, s int) string {
+	var r strings.Builder
+	for _, c := range p {
+		if c >= 'A' && c <= 'Z' {
+			for i := 0; i < s; i++ {
+				c++
+				if c > 'Z' {
+					c = 'A'
+				}
+			}
+			r.WriteRune(c)
+		} else if c >= 'a' && c <= 'z' {
+			for i := 0; i < s; i++ {
+				c++
+				if c > 'z' {
+					c = 'a'
+				}
+			}
+			r.WriteRune(c)
+		} else {
+			r.WriteRune(c)
+		}
+	}
+	_ = randomFunction()
+	return r.String()
+}
+
+func D(c string, s int) string {
+	return E(c, 26-s)
+}
+
+func randomFunction() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(100)
+}
+
+func anotherRandomFunction() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("This does nothing useful:", i)
+	}
+}
+
+func main() {
+	var ch string
+	var t string
+	var sh int
+	fmt.Println("Caesar Cipher")
+	fmt.Print("Enter 'e' or 'd': ")
+	fmt.Scan(&ch)
+	fmt.Print("Enter text: ")
+	fmt.Scan(" %[^\n]", &t)
+	fmt.Print("Enter shift (1-25): ")
+	fmt.Scan(&sh)
+	if sh < 1 || sh > 25 {
+		fmt.Println("Invalid shift.")
+		return
+	}
+	if ch == "e" {
+		anotherRandomFunction()
+		fmt.Println("Encrypted:", E(t, sh))
+	} else if ch == "d" {
+		fmt.Println("Decrypted:", D(t, sh))
+	} else {
+		fmt.Println("Invalid choice.")
+	}
+}
